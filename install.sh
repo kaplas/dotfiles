362c6d75785f74afcb1b0c4fd1c42239e3fe633a
Jouni Kaplas <jouni.kaplas@iki.fi>
2020-01-22 10:22:48 +0200

    Add asdf and defaultbrowser

--- install.sh
+++ install.sh
@@ -21,15 +21,24 @@ next()
   comment $@
 }
 
+# Helpers for combining the above
+brew_install()
+{
+	next "Install $@"
+	brew install $1
+	brew upgrade $1
+}
+
 ########################
   comment "Installing all necessary tools in kaplas's toolbox"
 ########################
 
 ########################
-  next "Install tmux"
+  next "Install some development tools..."
 ########################
-brew install tmux
-brew upgrade tmux
+
+brew_install tmux "for window management"
+brew_install asdf "for managing versions of different programming environments"
 
 ########################
   next "Configure tmux"
@@ -42,7 +51,21 @@ ln -s -f "$DOTFILES/configs/.tmux.conf.local" ~/.tmux.conf.local
 ls -l ~/.tmux.conf*
 
 ########################
-  next "Install elm"
+  next "Install development languages..."
 ########################
-brew install elm
-brew upgrade elm
+
+brew_install elm
+
+########################
+  #next "Install erlang and elixir using asdf"
+########################
+#asdf plugin-add erlang
+#asdf plugin-add elixir
+#asdf install erlang 22.2
+#asdf install elixir 1.9
+
+########################
+  next "Install work environment related tools..."
+########################
+
+brew_install defaultbrowser "for setting default browser for work days"
